d2844d27c1e8a9669d1414a1652aff1d3c99f3b1
DTreshy <szczukowski.damian@gmail.com>
2021-03-20 10:53:15 +0100

    Installed aws-S3 upload provider and updated the config files

--- config/plugins.js
+++ config/plugins.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'aws-s3',
     providerOptions: {
-      accessKeyId: env('AKIAUJOJBQDD3KHECXVV'),
-      secretAccessKey: env('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'),
-      region: env('eu-central-1'),
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: env('AWS_REGION'),
       params: {
-        Bucket: env('images.macahernandez.co'),
+        Bucket: env('AWS_BUCKET_NAME'),
       },
     },
   },
